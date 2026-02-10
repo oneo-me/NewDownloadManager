829ce1645c6b476ac9477c65bf223d79137a194c
ONEO <x@oneo.me>
2026-02-10 22:26:07 +0800

    Initial Commit

---
+++ NewDownloadManager/NewDownloadManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewDownloadManagerApp.swift
+//  NewDownloadManager
+//
+//  Created by ONEO on 2026/2/10.
+//
+
+import SwiftUI
+
+@main
+struct NewDownloadManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
